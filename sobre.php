1e12ca8d3d351373cd5e7eab7b6fe57c7f3e51cf
Your Name <you@example.com>
2024-06-27 19:32:52 -0300

    ..

---
+++ sobre.php
@@ -0,0 +1,20 @@
+<?php
+
+include "header.php";
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>Saiba mais sobre nós</h2>
+            <p>Conheça mais sobre nossa empresa</p>
+        </div>
+    </div>
+</div>
+
+<?php
+
+include "footer.php";
+
+?>
